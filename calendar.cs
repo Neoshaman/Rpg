ce30614ad70c45c05d1af4b7b1c7e05a5ae90a0a
Neoshaman <37513426+Neoshaman@users.noreply.github.com>
2018-03-20 07:39:43 -0300

    Add files via upload

---
+++ calendar.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class calendar : MonoBehaviour {
+
+	//24 mn, 1mn = 1h
+
+	//3 tiers:
+	//moonlight/daylight: 12h each
+	//sunrise/day/sunset/twilight: 6h each
+	//dawn/morning/afternoon/mid afternoon/dusk/evening/midnight/small hours: 3h each
+
+	//base routine: (bed table desk)
+	//6h - wake up/breakfast (bed, table)
+	//9h - begining work (desk)
+	//12h - lunch break (table)
+	//18h - going home/dinner (table)
+	//21h - sleeping (bed)
+
+	//each update, increment overall time, mod time and output tiers with percent completion
+
+	//newTime = time.now
+	//intervalTime = newTime - oldTime
+	//oldTime = newTime
+	//gametime += intervalTime
+	public float deltatime, unscaleddeltatime, basetick;//time in second fraction
+	public int milisecond, second, minute, hour, day, week, month, years;
+	public int gamesecond, gameminute, gamehour, gameday, gameweek, gamemonth, gameyears;
+	public int displaygamesecond, displaygameminute, displaygamehour, displaygameday, displaygameweek, displaygamemonth, displaygameyears;
+
+	void Start(){
+		basetick = 350;
+	}
+
+	void Update(){
+		deltatime = Time.deltaTime;
+		unscaleddeltatime = Time.unscaledDeltaTime;
+
+		//TODO: make code to pause the basetick
+		basetick += deltatime;
+
+		//real time
+		//TODO: should separate incrementation from basetick past the minutes, to avoid float precision
+		//ie reset the basetick after some cycle.
+
+		milisecond = (int) (basetick  * 1000f);
+		second = (int) basetick;
+		minute = second / 60; 
+		hour = minute / 60; 
+		day = hour / 24;
+		week = day / 7;
+		//month = week / 4; //need complex
+
+		//game time
+		gameminute = second;
+		gamehour = minute;
+		gameday = gamehour / 24;
+		gameweek = gameday / 7;
+
+		//display time SHOULD BE FLOAT for interpolation
+		displaygameminute = gameminute % 60;
+		displaygamehour = gamehour % 24;
+		displaygameday = gameday % 7;
+
+	}
+
+}
---
+++ schedule.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class schedule : MonoBehaviour {
+	public List<Task> behavior;
+	//public Transform destination;
+	public GameObject timecounter;
+
+	calendar timecount;
+	UnityEngine.AI.NavMeshAgent agent;
+	Animator anim;
+
+	public int debughour;
+	public int debugminute;
+
+
+	[System.Serializable]
+	public struct Task{
+		public string name;
+		public int starthour;
+		public int endhour;
+
+		public int duration;
+		public int findDuration(){
+			return duration = endhour - starthour;
+		}
+
+		public Transform location; //TODO:list of waypoints
+	}
+
+
+	//check time, if time go point
+
+	//24 mn, 1mn = 1h
+
+	//3 tiers:
+	//moonlight/daylight: 12h each
+	//sunrise/day/sunset/twilight: 6h each
+	//dawn/morning/afternoon/mid afternoon/dusk/evening/midnight/small hours: 3h each
+
+	//base routine: (bed table desk)
+	//6h - wake up/breakfast (bed, table)
+	//9h - begining work (desk)
+	//12h - lunch break (table)
+	//18h - going home/dinner (table)
+	//21h - sleeping (bed)
+
+	//state:
+	//sleeping
+	//waking up
+	//standing up from bed
+	//laying on bed
+
+	//walking
+
+	//sitting
+	//eating at table
+	//standing up from sit
+	//working on desk
+
+	//task:
+	//location
+	//start
+	//stop
+	//action (animation/goto/state)
+
+	//if  start =< time > stop AND NOT at location then do task, return
+
+	void Start(){
+		agent = GetComponent<UnityEngine.AI.NavMeshAgent> ();
+		anim = GetComponent<Animator> ();
+
+		timecount = timecounter.GetComponent<calendar> ();
+
+
+		int i = 0; while(i < behavior.Count){
+			Task t = behavior [i];
+
+			t.duration = t.findDuration();
+			i++; Debug.Log (t.duration);
+		}
+	}
+
+	void Update(){
+		
+
+		//TODO: only check at the end of each task, compute percent of interval
+		foreach (Task t in behavior) {
+			if (t.starthour <= timecount.displaygamehour & timecount.displaygamehour < t.endhour)
+				agent.SetDestination (t.location.position);
+			//TODO:if element is last take care of the looping comparison, look at manual patrol example of looping
+
+			//TODO:anim.
+		}
+		debughour = timecount.displaygamehour;
+		debugminute = timecount.displaygameminute;
+
+	}
+
+}
